c7b57110927af45b7a4a7a97bf1f69c9afadc45c
¨lautaro <shortlauty35@gmail.com>
2026-03-07 20:43:11 -0300

    se añadio el archivo consigna 2 donde contiene las funciones con tipado que requeria la actividad a realizar

---
+++ src/Consigna-2.ts
@@ -0,0 +1,6 @@
+// Funciones con tipado
+function updateStock(currentStock: number, quantityChange: number): number {
+  return currentStock + quantityChange;
+}
+
+console.log(updateStock(10, 30));
